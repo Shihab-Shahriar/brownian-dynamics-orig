12b086946678f33014abe26f36c31cb57bca5316
shihab-shahriar <bsse0703@iit.du.ac.bd>
2023-09-06 02:20:47 -0400

    works-2.22s

--- brownian.cpp
+++ brownian.cpp
@@ -4,8 +4,9 @@
 #include <vector>
 
 #include <hip/hip_runtime.h>
-#include <curand_kernel.h>
 
+#include <Kokkos_Core.hpp>
+#include <Kokkos_Random.hpp>
 
 #define FUNCTION_MACRO __host__ __device__
 #define PI           3.14159265358979323846 
@@ -24,7 +25,8 @@ void check_hip(hipError_t result, char const *const func, const char *const file
     }
 }
 
-typedef curandStatePhilox4_32_10_t RNG; // HIP random number generator type
+using ExecutionSpace = Kokkos::Cuda;
+using CudaMemorySpace = ExecutionSpace::memory_space;
 
 const double RADIUS = 1.0;
 const int N = 1000000;
@@ -63,37 +65,38 @@ struct Particle {
     }
 };
 
-__global__ void rand_init(RNG *rand_state) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    if(i >= N) 
-        return;
+// __global__ void rand_init(RNG *rand_state) {
+//     int i = threadIdx.x + blockIdx.x * blockDim.x;
+//     if(i >= N) 
+//         return;
 
-    // TODO: Each thread gets different seed, same sequence for
-    // performance improvement of about 2x!
-    curand_init(1984, i, 0, &rand_state[i]);
-}
+//     // TODO: Each thread gets different seed, same sequence for
+//     // performance improvement of about 2x!
+//     curand_init(1984, i, 0, &rand_state[i]);
+// }
 
-__global__ void init_particles(Particle *particles, RNG * rand_state) {
+__global__ void init_particles(Particle *particles, Kokkos::Random_XorShift64_Pool<CudaMemorySpace> random_pool) {
     int i = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
     if(i >= N) return;
 
     Particle p = particles[i];
     p.pid = i;
 
-    RNG local_rand_state = rand_state[i];
+    Kokkos::Random_XorShift64<CudaMemorySpace> gen =  random_pool.get_state();
 
-    auto x = curand_uniform_double(&local_rand_state) * double(windowWidth) - 1.0;
-    auto y = curand_uniform_double(&local_rand_state) * double(windowHeight) - 1.0;
+    auto x = gen.drand() * double(windowWidth) - 1.0;
+    auto y = gen.drand() * double(windowHeight) - 1.0;
     p.update(x, y);
 
-    p.vx = curand_uniform_double(&local_rand_state) * 100. - 50.0;
-    p.vy = curand_uniform_double(&local_rand_state) * 100. - 50.0;
+    p.vx = gen.drand() * 100. - 50.0;
+    p.vy = gen.drand() * 100. - 50.0;
 
-    rand_state[i] = local_rand_state;
+    random_pool.free_state(gen);
     particles[i] = p;
 }
 
-__global__ void apply_forces(Particle *particles, RNG * rand_state, double sqrt_dt, int counter) {
+__global__ void apply_forces(Particle *particles, Kokkos::Random_XorShift64_Pool<CudaMemorySpace> random_pool,
+         double sqrt_dt, int counter) {
     int i = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
     if(i >= N) return;
 
@@ -103,12 +106,12 @@ __global__ void apply_forces(Particle *particles, RNG * rand_state, double sqrt_
     p.vy -= GAMMA / mass * p.vy * dt;
 
     // Apply random force
-    RNG local_rand_state = rand_state[i];
+    Kokkos::Random_XorShift64<CudaMemorySpace> gen =  random_pool.get_state();
     
-    p.vx += (curand_uniform_double(&local_rand_state)  * 2.0 - 1.0) * sqrt_dt;
-    p.vy += (curand_uniform_double(&local_rand_state) * 2.0 - 1.0) * sqrt_dt;
+    p.vx += (gen.drand() * 2.0 - 1.0) * sqrt_dt;
+    p.vy += (gen.drand() * 2.0 - 1.0) * sqrt_dt;
     
-    rand_state[i] = local_rand_state;
+    random_pool.free_state(gen);
     particles[i] = p;
 }
 
@@ -131,32 +134,31 @@ __global__ void update_positions(Particle *particles) {
     particles[i] = p;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    Kokkos::ScopeGuard guard(argc, argv);
+
     const double sqrt_dt = std::sqrt(2.0 * T * GAMMA / mass * dt); // Standard deviation for random force
     std::cout << "sqrt_dt: " << sqrt_dt << "\n";
 
     const double density = (N * PI * RADIUS* RADIUS) / (windowWidth * windowHeight);
     std::cout << "density: " << density << "\n";
 
-    RNG *d_rand_states;
-    checkHipErrors(hipMalloc((void **)&d_rand_states, N*sizeof(RNG)));
+    Kokkos::Random_XorShift64_Pool<CudaMemorySpace> random_pool(12345);
 
     Particle *particles;
     checkHipErrors(hipMallocManaged((void **)&particles, N * sizeof(Particle)));
 
     const int nthreads = 256;
     const int nblocks = (N + nthreads - 1) / nthreads;
-    hipLaunchKernelGGL(rand_init, dim3(nblocks), dim3(nthreads), 0, 0, d_rand_states);
-    checkHipErrors(hipGetLastError());
-    checkHipErrors(hipDeviceSynchronize());
 
-    hipLaunchKernelGGL(init_particles, dim3(nblocks), dim3(nthreads), 0, 0, particles, d_rand_states);
+
+    hipLaunchKernelGGL(init_particles, dim3(nblocks), dim3(nthreads), 0, 0, particles, random_pool);
     checkHipErrors(hipGetLastError());
     checkHipErrors(hipDeviceSynchronize());
 
     int iter = 0;
     while (iter++ < STEPS) {
-        hipLaunchKernelGGL(apply_forces, dim3(nblocks), dim3(nthreads), 0, 0, particles, d_rand_states, sqrt_dt, iter);
+        hipLaunchKernelGGL(apply_forces, dim3(nblocks), dim3(nthreads), 0, 0, particles, random_pool, sqrt_dt, iter);
         checkHipErrors(hipGetLastError());
         checkHipErrors(hipDeviceSynchronize());
 
